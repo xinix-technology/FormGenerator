ec3d8e4c1e96f030013052114d580357ec93f664
Moh Ichsan <ichan0220@gmail.com>
2016-04-20 12:23:39 +0700

    templating Form & Checkout

--- src/FormGenerator/HtmlTemplate/Form.php
+++ src/FormGenerator/HtmlTemplate/Form.php
@@ -4,10 +4,10 @@ namespace FormGenerator\HtmlTemplate;
 
 class Form{
 	public function templateStart(){
-		return  '<form [id] [action] [class] [attributes]><div class="listViewContainer">[form_wrapper_open]<label for="">[label]</label><ul class="listText">';
+		return  '<form [id] [action] [class] [attributes]><div class="listViewContainer">[form_wrapper_open]<label for="">[label]</label><ol class="listText">';
 	}
 	public function templateEnd(){
-		return '</ul>[form_wrapper_close]</div><div class="buttonArea">[submit]</div></form>';
+		return '</ol>[form_wrapper_close]</div><div class="buttonArea">[submit]</div></form>';
 	}
 }
 
